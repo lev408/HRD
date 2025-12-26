441a72e654c229796f65036a89b142c517b2cbfa
lev408 <157982525+lev408@users.noreply.github.com>
2025-12-26 01:42:15 +0100

    Alt

--- GUI.py
+++ GUI.py
@@ -18,25 +18,6 @@ def star_data(name_star, temperature, magnitude, spectrum_raw_path, spectrum_pat
     magnitude.pack(side="top", anchor="nw")
     temperature.pack(side="top", anchor="nw")
 
-    def resize_image(new_width, new_height, img):
-        # Resize the image (maintaining aspect ratio)
-        ratio = min(new_width / img.width, new_height / img.height)
-        new_size = (int(img.width * ratio), int(img.height * ratio))
-        resized_img = img.resize(new_size, Image.LANCZOS)
-        return ImageTk.PhotoImage(resized_img)
-
-    def fit_images_to_window_area(event):
-        new_width = event.width
-        new_height = event.height
-
-        new_spectrum_image = resize_image(new_width, new_height, image_spectrum)
-        label_spectrum.config(image=new_spectrum_image)
-        label_spectrum.image = new_spectrum_image
-
-        new_spectrum_raw_image = resize_image(new_width, new_height, image_spectrum_raw)
-        label_spectrum_raw.config(image=new_spectrum_raw_image)
-        label_spectrum_raw.image = new_spectrum_raw_image
-
     image_spectrum = Image.open(spectrum_path)
     photo_spectrum = ImageTk.PhotoImage(image_spectrum)
     label_spectrum = ttk.Label(root, text="ausgewertetes Spektrum", image=photo_spectrum, compound="bottom",
@@ -49,7 +30,5 @@ def star_data(name_star, temperature, magnitude, spectrum_raw_path, spectrum_pat
 
     label_spectrum_raw.pack(side="top", anchor="nw")
     label_spectrum.pack(side="top", anchor="nw")
-
-    root.bind("<Configure>", fit_images_to_window_area)
 
     root.mainloop()
